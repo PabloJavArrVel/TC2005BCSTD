c7745cffb5c655777546492b9e2bece6e7680295
Pablo Javier Arreola Velasco <A01747824@tec.mx>
2023-02-23 12:30:58 -0600

    Asyncronous sort

--- Políticas del curso/app.js
+++ Políticas del curso/app.js
@@ -1,3 +1,11 @@
 console.log("Node");
 const fylesystem = require('fs');
 fylesystem.writeFileSync("Hola.txt","Hola desde node");
+
+const arreglo =[10,61,60,2000,5000,1000,10000]
+
+for (let item of arreglo){
+    setTimeout(() =>{
+        console.log(item);
+    },item);
+}
